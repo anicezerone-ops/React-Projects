de0553ae8e0d11d24bc4e1157f6e861be4502f31
anice <anice.t@zerone-consulting.com>
2026-06-08 17:38:58 +0530

    dnd-kit

---
+++ DNDKIT/dndkit/src/components/Column/Column.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Column.css";
+import {
+  SortableContext,
+  verticalListSortingStrategy
+} from "@dnd-kit/sortable";
+import Task from "../Task/Task";
+
+const Column = ({ tasks }) => {
+  return (
+    <div className="column">
+      <SortableContext items={tasks} strategy={verticalListSortingStrategy}>
+        {tasks.map((task) => (
+          <Task key={task.id} id={task.id} title={task.title} />
+        ))}
+      </SortableContext>
+    </div>
+  );
+};
+
+export default Column;
